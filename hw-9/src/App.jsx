7519700f9a538131c5e5108275ded367d5d9fc41
davcevva <163914445+davcevva@users.noreply.github.com>
2024-10-24 22:34:03 +0200

    hw

--- hw-9/src/App.jsx
+++ hw-9/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import FormWithLocalStorage from "./components/formDetails";
+import FormDetails from "./components/formDetails";
 
 function App() {
   return (
     <div>
-      <FormWithLocalStorage />
+      <FormDetails />
     </div>
   );
 }
